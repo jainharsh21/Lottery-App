c590b7c5ef44d93da81ffb50f2dc32a61d45c55a
HARSH JAIN <jainharsh270@gmail.com>
2020-04-08 18:11:17 +0530

    Updates

---
+++ precache-manifest.6b6caa3c2ba1706c74ada95143c784d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa99960f36c9218963d82e5d7442a96e",
+    "url": "/Lottery-App/index.html"
+  },
+  {
+    "revision": "6289c3bbfcf7fb8bf002",
+    "url": "/Lottery-App/static/css/main.b65e61ec.chunk.css"
+  },
+  {
+    "revision": "f08b56011e10dd8c514a",
+    "url": "/Lottery-App/static/js/2.71a52293.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Lottery-App/static/js/2.71a52293.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6289c3bbfcf7fb8bf002",
+    "url": "/Lottery-App/static/js/main.f68ec976.chunk.js"
+  },
+  {
+    "revision": "efd67b9afb973ad3081f",
+    "url": "/Lottery-App/static/js/runtime-main.e293ea97.js"
+  }
+]);
