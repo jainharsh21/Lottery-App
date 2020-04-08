a1178a70054473415596c5c3e11d0989f69a4a32
HARSH JAIN <jainharsh270@gmail.com>
2020-04-08 18:07:54 +0530

    added lottery component and styled it

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./App.css";
+import LotteryBall from "./LotteryBall";
+import Lottery from "./Lottery";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Lottery />
+      <Lottery title="Mini-Daily" maxNum={10} numBalls={4} />
+    </div>
+  );
 }
 
 export default App;
